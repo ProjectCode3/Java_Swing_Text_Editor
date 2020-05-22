d63e3df7d887903bb9cf6b0054232654334efc87
Creator-Solutions <54483520+Creator-Solutions@users.noreply.github.com>
2020-05-22 23:44:03 +0200

    Create The frame with the getter and constructor class methods

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,4 +1,18 @@
 package Client;
 
+import javax.swing.*;
+
+import Server.Server;
+
 public class Client {
+    public static Server server;
+    public static JFrame frame = new JFrame();
+
+    public static void main(String[] args){
+        server = new Server(1280, 720, "NotePad", true);
+
+        frame.setSize(server.getWidth(), server.getHeight());
+        frame.setTitle(server.getTitle());
+        frame.setVisible(server.setFrameVisible());
+    }
 }
